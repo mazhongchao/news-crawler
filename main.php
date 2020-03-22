2074e276dcc7f2cee80d91b8220092d3a841a5f7
mazhongchao <mazhongchao@gmail.com>
2020-03-22 16:49:20 +0800

    :construction: work in progress

--- main.php
+++ main.php
@@ -23,10 +23,9 @@ if (count($options) < 1) {
     echo $usage;
     exit();
 }
-$task_name = $options['t'];
 
-$dbconfig = require "./config/db-config.php";
 $task = require "task.php";
+$task_name = $options['t'];
 
 if (isset($task[$task_name])) {
     $sites = $task[$task_name];
@@ -37,7 +36,7 @@ if (isset($task[$task_name])) {
             $detail_url = '';
             $dump_file = false;
             //only first page of list
-            if (array_key_exists('f', $options)) { //
+            if (array_key_exists('f', $options)) {
                 $rule['list_next_max'] = 0;
             }
             //only specific list
@@ -60,20 +59,16 @@ if (isset($task[$task_name])) {
         foreach ($sites as $site => $rule_file) {
             if (is_file($rule_file)) {
                 $rule = require $rule_file;
-                if (array_key_exists('f', $options)) { //
+                if (array_key_exists('f', $options)) {
                     $rule['list_next_max'] = 0;
                 }
                 work($rule);
             }
-            else {
-                continue;
-            }
-            unset($rule);
         }
     }
 }
 
-function work($rule, $detail_url = '')
+function work($rule, $detail_url = '', $dump_file = false)
 {
     print_r($rule);
     exit();
@@ -81,44 +76,55 @@ function work($rule, $detail_url = '')
     $ql = QueryList::getInstance();
     $ql->use(AbsoluteUrl::class);
 
+    $redis = new Redis();
+    $redis->connect('127.0.0.1', 6379);
+    $redis->select(10);
+
     $start = time();
     echo "strarting>>> ".$start."\n";
 
+    //only for single detail page
     if ($detail_url != '') {
-        //
+        $rt = QueryList::get($detail_url)->rules($rule)->queryData();
+        if ($dump_file) {
+            //write the data into file, not implement it yet.
+        }
+        else {
+            if (!is_collected($redis, $detail_url)) {
+                $db = new Medoo($dbconfig);
+                $db->insert("article", $rt[0]);
+                add_url_hash($redis, $rt[0]);
+            }
+        }
         exit();
     }
-
-    //列表第一页
-    $link_arr = QueryList::get($rule['list_url'])->rules($rule['list_rules'])->queryData();
 
     $articles = [];
     $created_at = time();
 
+    //列表第一页
+    $link_arr = QueryList::get($rule['list_url'])->rules($rule['list_rules'])->queryData();
     //列表第一页的所有详情页
     foreach ($link_arr as $key => $value) {
         $detail_link = $value['detail_link'];
-        $rt = QueryList::get($detail_link)->rules($rule['detail_rules'])->absoluteUrl($detail_link)->queryData();
-        $rt[0]['site'] = $rule['site_name'];
-        $rt[0]['source_url'] = $detail_link;
-        $rt[0]['source_url_md5'] = md5($detail_link);
-        $rt[0]['created_at'] = $created_at;
-        $articles[] = $rt[0];
-        //print_r($rt);
+        if (!is_collected($redis, $detail_url)) {
+            $rt = QueryList::get($detail_link)->rules($rule['detail_rules'])->absoluteUrl($detail_link)->queryData();
+            $rt[0]['site'] = $rule['site_name'];
+            $rt[0]['source_url'] = $detail_link;
+            $rt[0]['source_url_md5'] = md5($detail_link);
+            $rt[0]['created_at'] = $created_at;
+            $articles[] = $rt[0];
+        }
     }
-    unset($link_arr);
     //print_r($articles);
-
-    $db = new Medoo($dbconfig);
-    $db->insert("article", $articles);
-    //var_dump($db->error());
-    //echo $db->last_query();
+    unset($link_arr);
+    dump_to_db($articles);
+    add_url_hash($redis, $articles);
 
     //只处理列表第一页
     if ($rule['list_next_max']+0 <= 0) {
         exit();
     }
-    echo "finished>>> ".(time()-$start)." ms";
 
     //接下来的列表页及详情页
     /*
@@ -126,15 +132,48 @@ function work($rule, $detail_url = '')
         $max_page = $rule['list_next_max'];
         $next_url = $rule['list_next_url'];
         for ($i=1; $i<=$max_page; i++) {
+            $articles = [];
             $next_list_url = sprintf($next_url, $i);
             $link_arr = QueryList::get($next_list_url)->rules($site['list_rules'])->queryData();
-
             foreach ($link_arr as $key => $value) {
                 $detail_link = $value['detail_link'];
-                $rt = QueryList::get($detail_link)->rules($rule['detail_rules'])->absoluteUrl($detail_link)->queryData();
+                if (!is_collected($redis, $detail_url)) {
+                    $rt = QueryList::get($detail_link)->rules($rule['detail_rules'])->absoluteUrl($detail_link)->queryData();
+                    $rt[0]['site'] = $rule['site_name'];
+                    $rt[0]['source_url'] = $detail_link;
+                    $rt[0]['source_url_md5'] = md5($detail_link);
+                    $rt[0]['created_at'] = $created_at;
+                    $articles[] = $rt[0];
+                }
                 print_r($rt);
             }
+            dump_to_db($articles);
         }
     }
     */
+    echo "finished>>> ".(time()-$start)." ms";
+}
+
+function is_collected($redis, $url)
+{
+    if ($redis->hExists('url_hash', md5($url))) {
+        return true;
+    }
+    return false;
+}
+
+function add_url_hash($redis, $articles)
+{
+    foreach ($articles as $article) {
+        $redis->hset('url_hash', $article['source_url_md5'], $article['created_at']);
+    }
+}
+
+function dump_to_db($articles)
+{
+    $dbconfig = require "./config/db-config.php";
+    $db = new Medoo($dbconfig);
+    $db->insert("article", $articles);
+    //var_dump($db->error());
+    //echo $db->last_query();
 }
