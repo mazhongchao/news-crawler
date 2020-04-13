e52715c8495c4f17182393eebc8c922945e1f6ae
mazhongchao <mazhongchao@gmail.com>
2020-04-13 16:43:39 +0800

    :fire: deleting some files

--- config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'database_type' => 'mysql',
-    'database_name' => '',
-    'server' => '127.0.0.1',
-    'username' => '',
-    'password' => ''
-];
--- task.php
+++
@@ -1,9 +0,0 @@
-<?php
-$task = [
-    '3h' => [
-        'yjb' => 'rules/yjb_cn_rule.php',
-    ],
-    '6h' => [],
-    '12h' => [],
-];
-return $task;
