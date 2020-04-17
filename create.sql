4d6fcb5f17a63dbbe96f555a1176b9f8882e0f8d
mazhongchao <mazhongchao@gmail.com>
2020-04-17 09:56:08 +0800

    performing database related changes

--- create.sql
+++ create.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE `yjx` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE `<YOUR_DATABASE_NAME>` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE TABLE `article` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(200) NOT NULL DEFAULT '' COMMENT '原文标题',
   `author` varchar(200) NOT NULL DEFAULT '' COMMENT '原文作者',
-  `pub_date` char(19) NOT NULL DEFAULT '' COMMENT '原文发布时间，完整格式：2020-03-18 17:00:00',
+  `pub_date` char(30) NOT NULL DEFAULT '' COMMENT '原文发布时间，完整格式：2020-03-18 17:00:00',
   `origin` varchar(100) NOT NULL DEFAULT '' COMMENT '原文的来源',
-  `content` text NOT NULL COMMENT ' 原文正文',
+  `content` text NOT NULL COMMENT '正文',
   `summary` varchar(255) NOT NULL DEFAULT '' COMMENT '原文摘要',
   `site` varchar(100) NOT NULL DEFAULT '' COMMENT '采集站点名称',
   `source_url` varchar(255) NOT NULL DEFAULT '' COMMENT '原文url地址',
@@ -20,6 +20,7 @@ CREATE TABLE `article` (
   `tags` varchar(255) NOT NULL DEFAULT '' COMMENT '原文标签',
   `read_count` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '原文阅读数：0为未提供',
   `reprinted` varchar(10) NOT NULL DEFAULT '' COMMENT '原文是转载还是原创，空为不明确',
+  `media_list` json NOT NULL COMMENT '原文中所含media的url的列表',
   PRIMARY KEY (`id`),
   KEY `KEY1` (`title`),
   KEY `KEY2` (`pub_date`),
@@ -28,7 +29,7 @@ CREATE TABLE `article` (
   KEY `KEY5` (`created_at`),
   KEY `KEY6` (`synced_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-CREATE TABLE `yjx`.`media` (
+CREATE TABLE `media` (
   `id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
   `media_type` CHAR(10) NOT NULL DEFAULT '' COMMENT '资源类型：image, audio, video, doc',
   `media_src` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '资源url',
